3f845c733e38ac86709a27620e535febc824f25f
Aaron Shafovaloff <aaronshaf@gmail.com>
2012-10-05 19:14:00 -0600

    Gotta start somewhere...

---
+++ forcewatch.js
@@ -0,0 +1,12 @@
+(function($) {
+	$.fn.forcewatch = function(options) {
+		var settings = $.extend({
+			'duration': .9,
+			'callback': function() {}
+		}, options);
+
+		return this.each(function() {
+			
+		});
+	};
+})(jQuery);
